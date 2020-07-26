35d577e4067aa4a54f9c65ffbd719bcf4873911d
Gabriel  S <gabrielmarioseno@gmail.com>
2020-07-26 10:36:49 -0300

    Proyecto

--- src/main.js
+++ src/main.js
@@ -1,9 +1,20 @@
-import Vue from "vue";
-import App from "./App.vue";
-import "./registerServiceWorker";
+import Vue from 'vue'
+import App from './App.vue'
+import './registerServiceWorker'
+import '@/assets/css/tailwind.css' // el @ nos permite dar el directorio raiz es como el ~ en la termina
+import Chartkick from 'vue-chartkick'
+import Chart from 'chart.js'
+import { VueSpinners } from '@saeris/vue-spinners'
+import router from '@/router/'
+import { dollarFilter, percentFilter } from '@/filter'
 
-Vue.config.productionTip = false;
+Vue.use(VueSpinners)
+Vue.use(Chartkick.use(Chart))
+Vue.config.productionTip = false
+Vue.filter('dollar', dollarFilter)
+Vue.filter('percent', percentFilter)
 
 new Vue({
+  router: router,
   render: h => h(App)
-}).$mount("#app");
+}).$mount('#app')
---
+++ src/views/Home.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <bounce-loader
+      v-bind:loading="isLoadinf"
+      v-bind:color="'#68d391'"
+      v-bind:size="100"
+    />
+    <px-assets-table v-if="!isLoadinf" v-bind:assets="assets" />
+  </div>
+</template>
+
+<script>
+import api from '@/api'
+import PxAssetsTable from '@/components/PxAssetsTable'
+export default {
+  name: 'Home',
+  components: { PxAssetsTable },
+  data() {
+    return {
+      assets: [],
+      isLoadinf: false
+    }
+  },
+
+  created() {
+    this.isLoadinf = true
+    api
+      .getAssets()
+      .then(assets => (this.assets = assets))
+      .finally(() => (this.isLoadinf = false))
+  }
+}
+</script>
